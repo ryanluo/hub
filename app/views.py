294aed1940ecbab0af56cec777d843cc238bb3e8
Ryan Luo <rllluo@gmail.com>
2014-10-20 17:26:56 -0400

    Keep APIs secret using environment variables

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
+import os
+
 from app import app
-from app import config
+
 from flask import render_template, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
   return render_template('index.html',
-                         API_KEY=config.MAPS_API_KEY,
+                         API_KEY= os.environ['MAPS_API_KEY'],
                          FIREBASE_INTERFACE = url_for('static',filename='js/firebase_interface.js'),
                          MAP_INTERFACE = url_for('static',filename='js/map_interface.js'))
