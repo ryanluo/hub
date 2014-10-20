49e651bdc98fe90b8e6458f15f6d33ca8524a315
Ryan Luo <rllluo@gmail.com>
2014-10-20 19:44:56 -0400

    Heroku shenanigans...

--- app/views.py
+++ app/views.py
@@ -8,6 +8,6 @@ from flask import render_template, url_for
 @app.route('/index')
 def index():
   return render_template('index.html',
-                         API_KEY= os.environ['MAPS_API_KEY'],
+                         API_KEY = os.environ['MAPS_API_KEY'],
                          FIREBASE_INTERFACE = url_for('static',filename='js/firebase_interface.js'),
                          MAP_INTERFACE = url_for('static',filename='js/map_interface.js'))
