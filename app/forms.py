f9e8253c1397775223469b3ec2f5a74aaf825cca
Andy Russell <arussell123@gmail.com>
2014-10-27 15:55:49 -0700

    add registration route

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from flask.ext.wtf import Form
+from wtforms import TextField, PasswordField
+from wtforms.validators import Required, Email, EqualTo
+
+
+class RegisterForm(Form):
+    name = TextField('Name', validators=[Required()])
+    email = TextField('Email', validators=[Email()])
+    password = PasswordField('Password', validators=[Required()])
+    confirm = PasswordField(
+        'Confirm Password',
+        validators=[Required(),
+                    EqualTo('password', message='Passwords do not match.')])
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        # TODO: Make sure that the user does not exist
+        return True
--- app/views.py
+++ app/views.py
@@ -1,10 +1,18 @@
-import os
-
 from app import app
+from forms import RegisterForm
 
-from flask import render_template
+from flask import redirect, render_template, url_for
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    if form.validate():
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
