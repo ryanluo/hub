0edd6df6dadd647c9bd4c8335f81a760dffb7a2d
Ryan Luo <rllluo@gmail.com>
2014-10-28 01:14:50 -0700

    Moved the login callback to views

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask.ext.login import LoginManager
 
-
 app = Flask(__name__)
 app.config.from_object('app.config.Config')
 
@@ -9,11 +8,5 @@ import firebase
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(userid):
-    r = firebase.get('/users.json')
-    return r
 
 from app import views
